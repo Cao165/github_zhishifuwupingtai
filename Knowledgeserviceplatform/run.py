844c24688d390c4c24cf1d008af759136645a314
Caol65 <3095771289@qq.com>
2022-03-25 10:56:04 +0800

    第8次

--- Knowledgeserviceplatform/run.py
+++ Knowledgeserviceplatform/run.py
@@ -2,8 +2,8 @@ import pytest
 import os
 
 
-pytest.main(['../github_zhishifuwupingtai/Test/TestCase','-s',
-             '--alluredir=../github_zhishifuwupingtai/Report/report'])
+pytest.main(['E:\python-pycharm\pythonProject\github_zhishifuwupingtai\Knowledgeserviceplatform\Test\TestCase','-s',
+             '--alluredir=E:\python-pycharm\pythonProject\github_zhishifuwupingtai\Knowledgeserviceplatform\Report\\report'])
 
-os.system('allure generate ../github_zhishifuwupingtai/Report/report -0'
-          'E:\python-pycharm\pythonProject\github_zhishifuwupingtai\Report --clean')
+os.system('allure generate E:\python-pycharm\pythonProject\github_zhishifuwupingtai\Knowledgeserviceplatform\Report\\report -0'
+          'E:\python-pycharm\pythonProject\github_zhishifuwupingtai\Knowledgeserviceplatform\Report --clean')
